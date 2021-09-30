0d91d6c1535fbc3c391158b13383264c3080404a
Nicolas Miguenz <nmiguenz@soluciones.com.ar>
2021-09-30 18:17:09 -0300

    Falta componente de puntos y validar bien la encuesta. Pulir

--- src/app/componentes/chat/chat.component.ts
+++ src/app/componentes/chat/chat.component.ts
@@ -13,28 +13,29 @@ import { User } from 'src/app/clases/user';
 export class ChatComponent implements OnInit {
 
   public openChat = false;
-  @Input('isLogged') isLogged : any;
-  
-  mensaje:string = '';
-  elemento: any;
+  @Input('isLogged') isLogged: any;
+
+  mensaje: string = '';
+  elemento: any = document.getElementById('divMensajes');
+
 
   usuarioLogueado: User = new User();
-  
-  constructor( public chatSrv : ChatServiceService,
-              private auth: AuthService) {
+
+  constructor(public chatSrv: ChatServiceService,
+    private auth: AuthService) {
     this.chatSrv.cargarMensaje()
-                .subscribe(() => {
-                   
-                  // Le agrego el TimeOut para que le de tiempo a cargar los mensajes
-                  setTimeout(()=>{
-                    this.elemento.scrollTop = this.elemento.scrollHeight;
-                  },20)
-                });
-    
-    this.auth.isLoggedIn().subscribe(user =>{
-      if(!user){
+      .subscribe(() => {
+        // Le agrego el TimeOut para que le de tiempo a cargar los mensajes
+        setTimeout(() => {
+          this.scrollToTheLastElementByClass();
+          // this.elemento.scrollTop = this.elemento.scrollHeight;
+        }, 500)
+      });
+
+    this.auth.isLoggedIn().subscribe(user => {
+      if (!user) {
         return
-      }else{
+      } else {
         this.usuarioLogueado.iduser = user.uid;
         this.usuarioLogueado.email = user.email!;
       }
@@ -42,15 +43,28 @@ export class ChatComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    console.log(this.elemento)
     //agarro el elemento
     this.elemento = document.getElementById('divMensajes');
   }
 
 
-  enviarMensaje(){
+  enviarMensaje() {
     this.chatSrv.agregarMensaje(this.mensaje, this.usuarioLogueado)
-                .then( ()=>{ this.mensaje = ''})
-                .catch( (error) => console.error('Error al enviar', error));
+      .then(() => {
+        setTimeout(() => {
+          this.scrollToTheLastElementByClass();
+        }, 20);
+        this.mensaje = ''
+      })
+      .catch((error) => console.error('Error al enviar', error));
   }
 
+  scrollToTheLastElementByClass() {
+    var elements = document.getElementsByClassName('mensajeDiv');
+    let ultimo: any = elements[(elements.length - 1)];
+    let topPos = ultimo?.offsetTop;
+    //@ts-ignore
+    document.getElementById('divMensajes')?.scrollTop = topPos;
+  }
 }
